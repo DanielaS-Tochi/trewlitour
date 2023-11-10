10c8e544e56413690c7516b4bb6473fe9727a24e
DanielaSilvanaTochi <133720661+DanielaS-Tochi@users.noreply.github.com>
2023-11-10 20:16:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+var swiper = new Swiper( ".mySwiper",{
+    slidesPerView:1, 
+    spaceBetween: 30,
+    loop:true,
+    pagination: {
+        el:".swiper-pagination",
+        clickeable: true,
+    },
+    navigation: {
+        nextEl:".swiper-button-next",
+        prevEl:".swiper-button-prev",
+    }
+    
+});
+
+var swiper = new Swiper( ".mySwiper-2", {
+    slidesPerView:3, 
+    spaceBetween: 20,
+    loop:true,
+    loopFillGroupWithBlank:true,
+    navigation: {
+        nextEl:".swiper-button-next",
+        prevEl:".swiper-button-prev",
+    },
+    brekpoints : {
+        0: {
+            slidesPerView:1,
+        },
+        520: {
+            slidesPerView:2,
+        },
+        950: {
+            slidesPerView:3,
+        },
+    }
+});
+
+let tabInouts = document.querySelectorAll(".tabinput");
+
+tabInouts.forEach(function(input) {
+  
+    unput.addEventListener('change', function() {
+        let id = imput.ariaValueMax;
+        let thisSwiper = document.getElementById('swiper' + id)
+        thisSwiper.swiper.update();
+    })
+});
